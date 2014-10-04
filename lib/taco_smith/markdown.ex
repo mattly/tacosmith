093811e2e3dc65fe15583afd2a61b52416409a24
Matthew Lyon <matthew@lyonheart.us>
2014-10-04 11:56:20 -0400

    Markdown.render has default %Earmark.Options{}

--- lib/taco_smith/markdown.ex
+++ lib/taco_smith/markdown.ex
@@ -4,10 +4,12 @@ defmodule TacoSmith.Markdown do
 
   def render(collection, earmark_options = %Earmark.Options{}) do
     filter = ~r/\.(md|markdown)$/
-    TacoSmith.process_each(collection, filter, &(render_markdown(&1, earmark_options)))
+    TacoSmith.process(collection, filter, &(render_record(&1, earmark_options)))
   end
 
-  def render_markdown(record = %TacoSmith.Content{}, earmark_options = %Earmark.Options{}) do
+  def render_record(record = %TacoSmith.Content{}), do: render_record(record, %Earmark.Options{})
+
+  def render_record(record = %TacoSmith.Content{}, earmark_options = %Earmark.Options{}) do
     body = record.body |> Enum.join |> Earmark.to_html(earmark_options)
     new_name = String.replace(record.info.path, Path.extname(record.info.path), ".html")
     info = Dict.put(record.info, :path, new_name)
