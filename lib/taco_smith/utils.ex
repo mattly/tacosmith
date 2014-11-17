d75ba3ef711b3c5319ee79851584a07fa27426f6
Matthew Lyon <matthew@lyonheart.us>
2014-11-16 20:16:32 -0800

    Date helper uses Timex

--- lib/taco_smith/utils.ex
+++ lib/taco_smith/utils.ex
@@ -1,26 +1,17 @@
 defmodule TacoSmith.Utils do
   import String, only: [to_integer: 1]
 
-  @date_pattern "(\\d{4})-(\\d{2})-(\\d{2})"
+  use Timex
 
-  @date_re Regex.compile!("^#{@date_pattern}$")
-
-  @time_pattern "(\\d{2}):(\\d{2})"
+  @date_format  "\\d{4}-\\d{1,2}-\\d{1,2}"
+  @date_regex   Regex.compile!("^#{@date_format}$")
+  @time_24h     Regex.compile!("^#{@date_format} \\d{1,2}:\\d{2}$")
 
   def date(str) do
     cond do
-      match = Regex.run(@date_re, str) ->
-        [_, y, m, d] = match
-        to_date(y, m, d)
-
-      match = Regex.run(~r/^#{@date_pattern}[ T]#{@time_pattern}$/, str) ->
-        [_, y, m, d, h, mi] = match
-        { to_date(y, m, d), to_time(h, mi, "0") }
+      Regex.run(@date_regex, str) -> DateFormat.parse!(str, "{YYYY}-{M}-{D}")
+      Regex.run(@time_24h, str) -> DateFormat.parse!(str, "{YYYY}-{M}-{D} {h24}:{m}")
     end
   end
-
-  defp to_date(y, m, d), do: {to_integer(y), to_integer(m), to_integer(d)}
-
-  defp to_time(h, m, s), do: {to_integer(h), to_integer(m), to_integer(s)}
 
 end
--- test/utils_test.exs
+++ test/utils_test.exs
@@ -1,14 +1,15 @@
 defmodule TacoSmithUtilsTest do
   use ExUnit.Case
+
+  use Timex
   alias TacoSmith.Utils
 
-  test "converts date strings to Date tuples" do
-    assert {2014, 1, 1} == Utils.date("2014-01-01")
+  test "converts date strings to Timex structs" do
+    assert Date.from({2014, 1, 1}) == Utils.date("2014-1-1")
   end
 
-  test "converts datetime strings to {Date, Time}" do
-    assert {{2014, 10, 20}, {13, 14, 0}} == Utils.date("2014-10-20 13:14")
-    # assert {{2014, 10, 20}, {13, 14, 0}} == Utils.date("2014-10-20T13:14")
+  test "converts datetime strings to Timex Structs" do
+    assert Date.from({{2014, 10, 20}, {13, 14, 0}}) == Utils.date("2014-10-20 13:14")
   end
 
 end
